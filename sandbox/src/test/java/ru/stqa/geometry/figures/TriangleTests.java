0f0c21dae346fc4bdd99fc8188a5b1074781a849
Stanislav <stas.starodubtsev@gmail.com>
2025-11-03 12:45:51 +0300

    Home task #2

---
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.geometry.figures;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TriangleTests {
+
+    @Test
+    void canCalculateTriangleArea(){
+        Assertions.assertEquals(14.5, new Triangle(5.0, 8.0, 12.0).area(), 0.1);
+    }
+
+    @Test
+    void canCalculateTrianglePerimeter(){
+        Assertions.assertEquals(25.0, new Triangle(5.0, 8.0, 12.0).perimeter());
+    }
+}
