d6690e49647583aef7424daff32d66c36bbcafcd
Stanislav <stas.starodubtsev@gmail.com>
2026-02-05 17:06:11 +0300

    Task #4

--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -34,4 +34,25 @@ public class TriangleTests {
             exception.printStackTrace();
         }
     }
+
+    @Test
+    void testEquality() {
+        var t1 = new Triangle(3.0,4.0, 5.0);
+        var t2 = new Triangle(3.0, 5.0, 4.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testEquality2() {
+        var t1 = new Triangle(3.0,4.0, 5.0);
+        var t2 = new Triangle(4.0, 5.0, 3.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testEquality3() {
+        var t1 = new Triangle(3.0,4.0, 5.0);
+        var t2 = new Triangle(4.0, 3.0, 5.0);
+        Assertions.assertEquals(t1, t2);
+    }
 }
